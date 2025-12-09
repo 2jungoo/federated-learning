30e8d567fdf630e39dbacddf25b35cfc03751fc8
creatorsg <soninseon2004@gmail.com>
2025-12-09 16:38:31 +0900

    Remove weight initialization method from server.py
    Removed the _initialize_weights method and its call.

--- server.py
+++ server.py
@@ -63,18 +63,6 @@ class Network1(nn.Module):
             nn.Linear(128, num_classes)
         )
 
-        self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-
     def forward(self, x):
         x = self.features(x)
         x = self.gap(x)
